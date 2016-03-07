86bffc434c10c9c8e9c35138f2ce4aebb2407fa9
DevPGSV <DevPGSV@gmail.com>
2016-03-07 18:16:14 +0100

    Implemented "createConsolePlayer", "createRandomPlayer" and "createAIPlayer".

--- es/ucm/fdi/tp/assignment4/ataxx/AtaxxFactory.java
+++ es/ucm/fdi/tp/assignment4/ataxx/AtaxxFactory.java
@@ -2,12 +2,17 @@ package es.ucm.fdi.tp.assignment4.ataxx;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
+import es.ucm.fdi.tp.basecode.bgame.control.ConsolePlayer;
+import es.ucm.fdi.tp.basecode.bgame.control.ConsolePlayerFromListOfMoves;
 import es.ucm.fdi.tp.basecode.bgame.control.Controller;
+import es.ucm.fdi.tp.basecode.bgame.control.DummyAIPlayer;
 import es.ucm.fdi.tp.basecode.bgame.control.GameFactory;
 import es.ucm.fdi.tp.basecode.bgame.control.Player;
 import es.ucm.fdi.tp.basecode.bgame.model.AIAlgorithm;
 import es.ucm.fdi.tp.basecode.bgame.model.GameError;
+import es.ucm.fdi.tp.basecode.bgame.model.GameMove;
 import es.ucm.fdi.tp.basecode.bgame.model.GameObserver;
 import es.ucm.fdi.tp.basecode.bgame.model.GameRules;
 import es.ucm.fdi.tp.basecode.bgame.model.Observable;
@@ -45,20 +50,22 @@ public class AtaxxFactory implements GameFactory {
 
 	@Override
 	public Player createConsolePlayer() {
-		// TODO Auto-generated method stub
-		return null;
+	    /*
+	    ArrayList<GameMove> possibleMoves = new ArrayList<GameMove>();
+        possibleMoves.add(new AtaxxMove());
+        return new ConsolePlayer(new Scanner(System.in), possibleMoves);
+        */
+	    return new ConsolePlayerFromListOfMoves(new Scanner(System.in));
 	}
 
 	@Override
 	public Player createRandomPlayer() {
-		// TODO Auto-generated method stub
-		return null;
+	    return new AtaxxRandomPlayer();
 	}
 
 	@Override
 	public Player createAIPlayer(AIAlgorithm alg) {
-		// TODO Auto-generated method stub
-		return null;
+	    return new DummyAIPlayer(createRandomPlayer(), 1000);
 	}
 
 	@Override
@@ -68,7 +75,6 @@ public class AtaxxFactory implements GameFactory {
 		pieces.add(new Piece("O"));
 		pieces.add(new Piece("R"));
 		pieces.add(new Piece("B"));
-		//pieces.add(AtaxxRules.getObstacle());
 		return pieces;
 	}
 
